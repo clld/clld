0937e8c26e5232705664bbb0ec804a8096d6d0f6
Robert Forkel <xrotwang@googlemail.com>
2013-10-09 17:04:29 +0200

    added support for reading rows in excel sheets

--- clld/lib/excel.py
+++ clld/lib/excel.py
@@ -1,8 +1,10 @@
 from six import PY3
 if not PY3:
     import xlwt
+    import xlrd
 else:  # pragma: no cover
     xlwt = None
+    xlrd = None
 
 
 def hyperlink(url, label=None):
@@ -16,3 +18,18 @@ def hyperlink(url, label=None):
     style.font = f
     label = label.replace('"', "'") if label else url
     return xlwt.Formula('HYPERLINK("%s";"%s")' % (url, label[:255]))
+
+
+def rows(sheet, as_dict=False):
+    if as_dict:
+        # we use the values of the first row as keys:
+        keys = [sheet.cell(0, i).value for i in range(sheet.ncols)]
+        start = 1
+    else:
+        start = 0
+
+    for j in range(start, sheet.nrows):
+        res = [sheet.cell(j, i).value for i in range(sheet.ncols)]
+        if as_dict:
+            res = dict(zip(keys, res))
+        yield res
