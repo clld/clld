c29894d1636a6d8e6a5a8159a8d4079b30b6c151
xrotwang <xrotwang@googlemail.com>
2014-02-10 11:47:26 +0100

    fixed scaffold and package

--- clld/web/app.py
+++ clld/web/app.py
@@ -530,7 +530,7 @@ def get_configurator(pkg, *utilities, **kw):
         config.add_translation_dirs('%s:locale' % config.package_name)
         config.add_translation_dirs('clld:locale')
 
-    if pkg_dir.joinpath('static/publisher_logo.png').exists():
+    if pkg_dir.joinpath('static/publisher_logo.png').exists():  # pragma: no cover
         config.add_settings(
             {'clld.publisher_logo': '%s:static/publisher_logo.png' % config.package_name})
 
