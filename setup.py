7a831ddf04c49a30409b89982c3822555e7a31da
xrotwang <xrotwang@googlemail.com>
2014-07-14 12:29:17 +0200

    bumbed version number

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ testing_extras = tests_require + [
     ]
 
 setup(name='clld',
-      version='0.13.1',
+      version='0.13.2',
       description=(
           'Python library supporting the development of cross-linguistic databases'),
       long_description='',
