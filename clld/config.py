be54a947ea3058816ddb431ccd6408e6f691c305
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2014-09-23 17:57:01 +0200

    refactored a little

--- clld/config.py
+++ clld/config.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals, print_function, division, absolute_import
 import codecs
+from functools import partial
 
 from six.moves.configparser import ConfigParser
 
@@ -15,16 +16,16 @@ def get_config(p):
     parser.readfp(codecs.open(p, encoding='utf8'))
 
     for section in parser.sections():
+        getters = {
+            'int': partial(parser.getint, section),
+            'boolean': partial(parser.getboolean, section),
+            'float': partial(parser.getfloat, section),
+            'list': lambda option: parser.get(section, option).split(),
+        }
+        default = partial(parser.get, section)
         for option in parser.options(section):
-            type_ = option.split('_')[-1] if '_' in option else None
-            value = {
-                'int': parser.getint,
-                'boolean': parser.getboolean,
-                'float': parser.getfloat,
-            }.get(type_, parser.get)(section, option)
-            value = {
-                'list': lambda v: v.split(),
-            }.get(type_, lambda v: v)(value)
+            type_ = option.rpartition('_')[2] if '_' in option else None
+            value = getters.get(type_, default)(option)
             cfg['{0}.{1}'.format(section, option)] = value
 
     return cfg
