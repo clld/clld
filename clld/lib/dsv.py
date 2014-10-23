143ab20b2fd9cbacdaa5c7ff0acced2a50584849
xrotwang <xrotwang@googlemail.com>
2014-10-23 10:28:32 +0200

    refactored to not interfere with test coverage

--- clld/lib/dsv.py
+++ clld/lib/dsv.py
@@ -198,10 +198,8 @@ class NamedTupleReader(UnicodeDictReader):
         d = UnicodeDictReader.item(self, row)
         for name in self.fieldnames:
             d.setdefault(name, None)
-        for k in list(d.keys()):
-            if k not in self.fieldnames:
-                del d[k]
-        return self.cls(**{normalize_name(k): v for k, v in d.items()})
+        return self.cls(
+            **{normalize_name(k): v for k, v in d.items() if k in self.fieldnames})
 
 
 def normalize_name(s):
