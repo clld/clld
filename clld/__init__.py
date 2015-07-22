acd609e27131847520b0127ed0593e4cb6bf539a
xrotwang <xrotwang@googlemail.com>
2015-07-22 17:00:24 +0200

    bumped version number

--- clld/__init__.py
+++ clld/__init__.py
@@ -5,7 +5,7 @@ from clld.db.models import common
 from clld import interfaces
 
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 
 
 class Resource(namedtuple('Resource', 'name model interface with_index with_rdfdump')):
