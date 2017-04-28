0de574f40c2563e49797fb00642e099eff83ac2f
xrotwang <xrotwang@googlemail.com>
2017-04-28 09:09:10 +0200

    closes #126

--- clld/web/static/js/clld.js
+++ clld/web/static/js/clld.js
@@ -364,7 +364,8 @@ CLLD.MapIcons = {
             iconUrl: url == undefined ? feature.properties.icon : url,
             iconSize: [size, size],
             iconAnchor: [Math.floor(size/2), Math.floor(size/2)],
-            popupAnchor: [0, 0]
+            popupAnchor: [0, 0],
+            className: feature.properties.class == undefined ? 'clld-map-icon' : feature.properties.class
         });
     }
 };
