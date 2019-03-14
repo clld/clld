02cd348d37f21755e6b26634549911f349b09c86
Christoph Rzymski <christoph@functor-argument.net>
2019-03-14 11:53:11 +0100

    bump version for development

--- src/clld/__init__.py
+++ src/clld/__init__.py
@@ -5,7 +5,7 @@ from clld.db.models import common
 from clld import interfaces
 
 
-__version__ = "4.5.0"
+__version__ = "4.5.1.dev0"
 
 
 class Resource(namedtuple('Resource', 'name model interface with_index with_rdfdump')):
