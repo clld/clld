3e9fbb48bdd6e66bc5f18489a5167238b3b65bc6
xrotwang <xrotwang@googlemail.com>
2019-06-03 18:28:43 +0200

    closes #186

--- src/clld/db/models/language.py
+++ src/clld/db/models/language.py
@@ -94,8 +94,7 @@ class IdentifierType(DeclEnum):
 
     """Known language identifiers."""
 
-    iso = 'iso639-3', 'ISO 639-3', \
-          'http://www-01.sil.org/iso639-3/documentation.asp?id={0.name}'
+    iso = 'iso639-3', 'ISO 639-3', 'https://iso639-3.sil.org/code/{0.name}'
     wals = 'wals', 'WALS Code', 'http://wals.info/languoid/lect/wals_code_{0.name}'
     glottolog = 'glottolog', 'Glottocode', \
                 'http://glottolog.org/resource/languoid/id/{0.name}'
