bace034f66789afb2f82b6ff714c2a7e27af22f7
xrotwang <xrotwang@googlemail.com>
2019-06-04 12:42:29 +0200

    release 4.7.0

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ install_requires = [
 
 setup(
     name='clld',
-    version='4.6.2.dev0',
+    version='4.7.0',
     description=(
         'Python library supporting the development of cross-linguistic databases'),
     long_description=open('README.md').read(),
