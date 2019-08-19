3345b6febb8b2ba475725ebef0a04749f58ecfaf
xrotwang <xrotwang@googlemail.com>
2019-08-19 11:38:23 +0200

    release 4.7.2

--- src/clld/__init__.py
+++ src/clld/__init__.py
@@ -5,7 +5,7 @@ from clld.db.models import common
 from clld import interfaces
 
 
-__version__ = "4.7.2.dev0"
+__version__ = "4.7.2"
 
 
 class Resource(namedtuple('Resource', 'name model interface with_index with_rdfdump')):
