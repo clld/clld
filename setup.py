388dc367ce56f90fab3fb1bded865513d2d2bac1
xrotwang <xrotwang@googlemail.com>
2019-09-17 21:23:45 +0200

    bump version for development

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ install_requires = [
 
 setup(
     name='clld',
-    version='5.0.0',
+    version='5.0.1.dev0',
     description=(
         'Python library supporting the development of cross-linguistic databases'),
     long_description=open('README.md').read(),
