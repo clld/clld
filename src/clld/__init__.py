75ff4c48ae1adfd58c77283691a7381108ff4869
xrotwang <xrotwang@googlemail.com>
2019-11-08 10:44:50 +0100

    bump version for development

--- src/clld/__init__.py
+++ src/clld/__init__.py
@@ -3,7 +3,7 @@ from collections import namedtuple
 from clld.db.models import common
 from clld import interfaces
 
-__version__ = "5.1.0"
+__version__ = "5.1.1.dev0"
 
 
 class Resource(namedtuple('Resource', 'name model interface with_index with_rdfdump')):
