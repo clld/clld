5e3d4565b75f2e01ef109d0385c52cb50d2df194
Robert Forkel <xrotwang@googlemail.com>
2021-03-31 10:15:17 +0200

    bump version for development

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ install_requires = [
 
 setup(
     name='clld',
-    version='7.4.2',
+    version='7.4.3.dev0',
     description=(
         'Python library supporting the development of cross-linguistic databases'),
     long_description=open('README.md').read(),
