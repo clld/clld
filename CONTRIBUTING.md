1fa4e68a0011f2a13ab8710d48d614881d47925b
Robert Forkel <xrotwang@googlemail.com>
2023-01-17 15:37:04 +0100

    going through the release procedure again

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -3,11 +3,17 @@ Contributing
 
 Development envionment:
 
-```sh
+```shell
 $ pip install virtualenv  # might require sudo/admin privileges
 $ git clone https://github.com/clld/clld.git
 $ cd clld
 $ python -m virtualenv .venv
 $ source .venv/bin/activate  # Windows: .venv\Scripts\activate.bat
 $ pip install -r requirements.txt  # installs the cloned version with dev-tools in development mode
+```
+
+In order to be able to run the javascript tests, `mocha` etc. must be installed in a node (>=18)
+environment. `npm` will pickup the relevant requirements from `package.json`:
+```shell
+$ npm install
 ```
--- tests/fixtures.cjs
+++ tests/fixtures.cjs
@@ -1,23 +1,10 @@
 
-
 exports.mochaGlobalSetup = async function () {
     this.jsdom = require('jsdom-global')()
     window.jQuery = global.$ = global.jQuery = require('../src/clld/web/static/js/jquery.js');
 
-    const webIDLConversions = require("webidl-conversions");
-
+    /* jsdom doesn't export a complete Option constructor yet. */
     global.Option = function(text, value, defaultSelected, selected) {
-        if (text === undefined) {
-            text = "";
-        }
-        text = webIDLConversions.DOMString(text);
-
-        if (value !== undefined) {
-            value = webIDLConversions.DOMString(value);
-        }
-
-        defaultSelected = webIDLConversions.boolean(defaultSelected);
-        selected = webIDLConversions.boolean(selected);
         return window._document.createElement("option");
     }
     Object.defineProperty(Option, "prototype", {
@@ -40,20 +27,10 @@ exports.mochaGlobalSetup = async function () {
     require("../src/clld/web/static/js/jquery.dataTables.js")(window, $);
     global.L = require("../src/clld/web/static/js/leaflet.js");
     require("../src/clld/web/static/js/L.Control.Resizer.js");
-    //"src/clld/web/static/js/Leaflet.fullscreen.js",
     require("../src/clld/web/static/js/leaflet-hash.js");
     require("../src/clld/web/static/js/leaflet-providers.js");
-    //const oms = require("../src/clld/web/static/js/oms.min.js");
-    //global.OverlappingMarkerSpiderfier = oms.OverlappingMarkerSpiderfier;
-    //"src/clld/web/static/js/raphael.min.js",
     require("../src/clld/web/static/js/select2.js");
-    //require("../src/clld/web/static/js/tree.jquery.js")(window);
-
-  //this.server = await startSomeServer({port: process.env.TEST_PORT});
-  //console.log(`server running on port ${this.server.port}`);
 };
 
 exports.mochaGlobalTeardown = async function () {
-  //await this.server.stop();
-  //console.log('server stopped!');
 };
